fe11322baf7804165b8609a0ff3ce41b0a469177
Yudhajeet Nandi <lenovo.tab.wrick@gmail.com>
2025-10-30 15:46:40 +0530

    feat: Embed YouTube videos using nocookie domain
    Update Content Security Policy and iframe source to use youtube-nocookie.com for embedded YouTube videos. This enhances user privacy by preventing YouTube from tracking user activity for personalization.

--- components/MusicLibrary.tsx
+++ components/MusicLibrary.tsx
@@ -65,7 +65,7 @@ const MusicLibrary: React.FC<{ className?: string }> = ({ className = '' }) => {
                         <iframe
                             width="100%"
                             height="100%"
-                            src={`https://www.youtube.com/embed/${nowPlaying.videoId}?autoplay=1`}
+                            src={`https://www.youtube-nocookie.com/embed/${nowPlaying.videoId}?autoplay=1`}
                             title={nowPlaying.title}
                             frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
